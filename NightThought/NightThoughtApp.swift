f4888338e147acff2ad20f5ad878cfc4127c1cff
Reoto A <hinohino0211@gmail.com>
2026-03-10 22:37:18 +0800

    Initial Commit

---
+++ NightThought/NightThoughtApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightThoughtApp.swift
+//  NightThought
+//
+//  Created by Ando Reoto on 2026/03/10.
+//
+
+import SwiftUI
+
+@main
+struct NightThoughtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NightThoughtTests/NightThoughtTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightThoughtTests.swift
+//  NightThoughtTests
+//
+//  Created by Ando Reoto on 2026/03/10.
+//
+
+import Testing
+@testable import NightThought
+
+struct NightThoughtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
